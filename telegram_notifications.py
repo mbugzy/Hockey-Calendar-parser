039d19e68402289af85da927871443b57b27a941
mbugzy <mbugzy087@gmail.com>
2026-01-26 21:28:35 +0300

    fix: minor code-style changes

--- telegram_notifications.py
+++ telegram_notifications.py
@@ -6,24 +6,27 @@ from logger import Logger
 import configparser
 
 logger = Logger(__name__)
+config = configparser.ConfigParser()
+config.read("urls.ini")
+TOKEN = config['telegram']['token']
+BASE_URL = f'https://api.telegram.org/bot{TOKEN}/'
 
-def get_telegram_credential(file_path: str) -> dict[str, str]:    
-    config = configparser.ConfigParser()
-    config.read(file_path)
-    credentials = config['telegram']
-    return credentials
+def reformat_wtih_markdown(text):
 
+    pass
 
 def send_notification(text: str):
-    credentials = get_telegram_credential("urls.ini")
-    chat_id = credentials['personal_chat_id']
-    token = credentials['personal_token']
     
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    chat_id = config['telegram']['personal_chat_id']
+
+    text = reformat_wtih_markdown(text)
+    
     payload = {
         "chat_id": chat_id,
         "text": text,
         "parse_mode": "Markdown"
     }
-    response = requests.post(url, json=payload)
+
+    response = requests.post(f"{BASE_URL}sendMessage", json=payload)
+
     return response.status_code == 200
