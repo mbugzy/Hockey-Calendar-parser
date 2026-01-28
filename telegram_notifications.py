e3a8cd0cc44df11171eb7e44fa9a8b281c3c6ba5
mbugzy <mbugzy087@gmail.com>
2026-01-28 21:55:58 +0300

    fix: typo, ARENAS and run on push

--- telegram_notifications.py
+++ telegram_notifications.py
@@ -11,15 +11,15 @@ config.read("urls.ini")
 TOKEN = config['telegram']['token']
 BASE_URL = f'https://api.telegram.org/bot{TOKEN}/'
 
-def reformat_wtih_markdown(text):
+def reformat_with_markdown(text):
 
-    pass
+    return text
 
 def send_notification(text: str):
     
     chat_id = config['telegram']['personal_chat_id']
 
-    text = reformat_wtih_markdown(text)
+    text = reformat_with_markdown(text)
     
     payload = {
         "chat_id": chat_id,
